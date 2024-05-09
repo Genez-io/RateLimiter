1677d976f6234588293686c7095eec14e35af359
Virgil993 <turcu.virgil@gmail.com>
2024-05-09 13:42:32 +0300

    published v1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import Redis from "ioredis";
 import { GenezioError, GenezioErrorCodes } from "@genezio/types";
 
+/**  *
+ * The type that defines the parameters of the GenezioRateLimiter decorator.
+ * @param {string} dbUrl The URL of the Redis database.
+ * @param {number} limit The maximum number of requests that can be made from the same sourceIp in a minute.
+ *
+ */
 export type GenezioRateLimiterOptionsParameters = { dbUrl?: string; limit?: number };
 
-// Decorator that marks that limits the number of requests being made from the same sourceIp.
+/**
+ * Decorator that marks that limits the number of requests being made from the same sourceIp.
+ * @param {GenezioRateLimiterOptionsParameters} _dict An object that contains the parameters of the GenezioRateLimiter.
+ * The object has the following structure: {dbUrl?: string, limit?: number}
+ * @returns the async function that will be executed after the rate limiter is applied.
+ */
 export function GenezioRateLimiter(_dict: GenezioRateLimiterOptionsParameters = {}) {
     let redisClient: Redis;
+    // Connect to the Redis database
     try {
         redisClient = new Redis(_dict.dbUrl ? _dict.dbUrl : "", {
             retryStrategy: function () {
@@ -20,6 +32,7 @@ export function GenezioRateLimiter(_dict: GenezioRateLimiterOptionsParameters = 
     }
     return function (value: Function, _context: any) {
         return async function (...args: any[]) {
+            // Check if the first parameter is a GnzContext object
             if (args.length === 0 || !args[0].isGnzContext) {
                 console.log(
                     "Warning: the GenezioRateLimiter decorator must be used with the first parameter being a GnzContext object"
@@ -30,18 +43,21 @@ export function GenezioRateLimiter(_dict: GenezioRateLimiterOptionsParameters = 
                     const oldCount = await redisClient.get(
                         `${args[0].requestContext.http.sourceIp}:${date.getMinutes()}`
                     );
+                    // If the limit is reached, throw an error
                     if (oldCount && parseInt(oldCount) >= (_dict.limit ? _dict.limit : 50)) {
                         throw new GenezioError(
                             "Rate limit exceeded",
                             GenezioErrorCodes.RequestTimeout
                         );
                     }
+                    // Increment the count of requests made by the sourceIp in the current minute
                     await redisClient
                         .multi()
                         .incr(`${args[0].requestContext.http.sourceIp}:${date.getMinutes()}`)
                         .expire(`${args[0].requestContext.http.sourceIp}:${date.getMinutes()}`, 59)
                         .exec();
                 } catch (error) {
+                    // If the error is a RequestTimeout error, throw it to indicate that the request should be timed out
                     if ((error as GenezioError).code === GenezioErrorCodes.RequestTimeout) {
                         throw error;
                     }
